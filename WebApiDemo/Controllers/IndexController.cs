361db3dc61edf00ddfdc97fc4b4a69d1ad76f9a7
BlackMesa-Canteen <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-07-17 00:00:20 +1000

    git add

---
+++ WebApiDemo/Controllers/IndexController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers;
+
+public class IndexController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        string value = "datatata";
+        ViewBag.value = value;
+        return View();
+    }
+}
---
+++ WebApiDemo/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers;
+
+[ApiController]
+public class TestController : ControllerBase
+{
+    // GET
+    [Route("api/[controller]")]
+    [HttpGet]
+    public string Get(string name)
+    {
+        return "Hello, " + name;
+    }
+}
